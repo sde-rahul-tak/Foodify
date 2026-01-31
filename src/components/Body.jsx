6f8f6244b7493f5793ef084c583a5cb4214f756d
Rahul Tak <codebase.rahul@gmail.com>
2026-01-31 23:32:22 +0530

    add tope rated filter feature with useState Hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,27 @@
 import restList from "../utills/mockData";
 import Res_Card from "./Res_Card";
+import mockData from "../utills/mockData"
+import { useState } from "react";
 
 const Body = ()=>{
+    const [listOfRestaurant, setListOfRestaurant] = useState(mockData);
   return (
     <div className="Body">
-      <div className="search">
-        <h4>Search</h4>
+      <div className="filter-btn">
+        <button onClick={
+            ()=>{
+                const filterData = listOfRestaurant.filter(
+                    (res)=>res.info.avgRatingString > 4.5
+                );
+                setListOfRestaurant(filterData)
+            }
+        }>
+            Top Rated Restaurant
+        </button>
       </div>
       <div className="res-container">
         {
-          restList.map((restaurant)=>(
+          listOfRestaurant.map((restaurant)=>(
             <Res_Card key={restaurant.info.id} restaurantData = {restaurant}/>
           ))
         }
