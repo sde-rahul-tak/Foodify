ae2391381e638f8f796b671c10da7bbe51bf2089
Rahul Tak <codebase.rahul@gmail.com>
2026-02-21 18:51:02 +0530

    useEffect and optional chainning or shimmer Ui

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,36 @@
-import restList from "../utills/mockData";
 import Res_Card from "./Res_Card";
-import mockData from "../utills/mockData"
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import ShimmerUi from "./ShimmerUI";
 const Body = ()=>{
-    const [listOfRestaurant, setListOfRestaurant] = useState(mockData);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  const fetchData = async()=>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING#");
+
+    const json = await data.json();
+    console.log(json);
+
+    const restaurantCard = json?.data?.cards?.find(
+    c => c?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  const restaurants =
+  restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+  setListOfRestaurant(restaurants)
+  }
+
+  if(listOfRestaurant.length === 0){
+    return <ShimmerUi/>
+  }
   return (
     <div className="Body">
       <div className="filter-btn">
         <button onClick={
             ()=>{
                 const filterData = listOfRestaurant.filter(
-                    (res)=>res.info.avgRatingString > 4.5
+                    (res)=>Number(res.info.avgRatingString) > 4.5
                 );
                 setListOfRestaurant(filterData)
             }
---
+++ src/components/ShimmerUI.jsx
@@ -0,0 +1,26 @@
+const ShimmerUi = ()=>{
+    return (
+    <div className="shimmer-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+    </div>
+    )
+}
+
+export default ShimmerUi
